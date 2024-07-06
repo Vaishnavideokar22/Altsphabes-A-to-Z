7d09c06b9c99931791057b368bee9b2b544611e8
Vaishnavideokar22 <vaishnavideokar33@gmail.com>
2024-07-06 14:34:55 +0530

    Add files via upload

---
+++ CharchterString.java
@@ -0,0 +1,20 @@
+class Charchter{
+    public void PrintCharchter(){
+        for (char i = 'A'; i <= 'Z'; i++ ){
+            System.out.println(i);
+        }
+
+    }
+}
+public class CharchterString {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Charchter A To Z are: ");
+
+        Charchter c1 = new Charchter();
+        c1.PrintCharchter();
+
+    }
+    
+}
